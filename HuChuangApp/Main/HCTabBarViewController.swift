677dc3156ff966f220d7ac88a4c5645f2b8a2376
swift <mlsy.swift@gmail.com>
2019-02-02 16:04:28 +0800

    sb完成

---
+++ HuChuangApp/Main/HCTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HCTabBarViewController.swift
+//  HuChuangApp
+//
+//  Created by sw on 02/02/2019.
+//  Copyright © 2019 sw. All rights reserved.
+//
+
+import UIKit
+
+class HCTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
